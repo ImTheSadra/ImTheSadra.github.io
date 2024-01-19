fe4c25e1b2210ed0d7ac57bdca36b68ddd4d91fd
sadra <m.sadra.gorji@gmail.com>
2024-01-19 16:21:53 +0400

    making

---
+++ script/game.js
@@ -0,0 +1,93 @@
+let mapSize, tileSize;
+let FOV = Math.PI/4;
+let HALF_FOV = FOV / 2;
+let castedRays = 120;
+let stepAngle = FOV / castedRays;
+let maxDepth, SCALE, wallWidth;
+
+playerx = 0;
+playery = 0;
+playera = 0;
+
+function toInt(num){
+    var result = Math.round(num);
+    if (result > num){
+        result += 1;
+    }
+    return result;
+}
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+function setup(){
+    createCanvas(700, 500);
+
+    // var main = document.getElementsByTagName("main").item(0);
+    // main.style = "display: none; max-width: 700; max-height: 500;";
+    // main.id = "main";
+
+    // main.className = "window";
+
+    // var window = document.getElementById("window");
+    // window.children = [main];
+
+    mapSize = 10;
+    tileSize = (width/3)/mapSize;
+    maxDepth = (mapSize*tileSize)*2;
+    SCALE = (width / 2) / castedRays;
+    wallWidth = width / castedRays;
+    playerx = mapSize/2*tileSize;
+    playery = mapSize/2*tileSize;
+}
+
+function castRays(){
+    angle = playera+HALF_FOV;
+
+    for (let ray = 0; ray < castedRays; ray++){
+        for(let depth = 0; depth < maxDepth; depth++){
+            let tx, ty;
+            tx = playerx-(Math.sin(angle)*depth);
+            ty = playery+(Math.cos(angle)*depth);
+            
+            row = tx / tileSize;
+            col = ty / tileSize;
+
+            var rrow = toInt(row);
+            var rcol = toInt(col);
+
+            if (map[rrow][rcol] >= 1){
+                wallHeight = 12000 / (depth + 0.0001);
+                wallWidth *= 2;
+
+                var startY = toInt(height-wallWidth) / 2;
+                var endY = toInt(height+wallWidth) / 2;
+
+                stroke(255);
+                strokeWeight(toInt(wallWidth));
+
+                line(
+                    ray * wallWidth, startY,
+                    ray * wallWidth, endY
+                );
+                break;
+            }
+        }
+        angle -= stepAngle;
+    }
+}
+
+function draw(){
+    background(10);
+    castRays()
+}
---
+++ script/main.js
@@ -0,0 +1,65 @@
+function clearBash(){
+    var bash = document.getElementById("bash-promp");
+    bash.innerHTML = "";
+    bash.innerText = "";
+    var bash = document.getElementById("bash-promp2");
+    bash.innerHTML = "";
+    bash.innerText = "";
+
+    var bash = document.getElementById("bash-promp2-view");
+    bash.style = "display: none;";
+
+    var result = document.getElementById("result");
+    result.style = "display: none";
+}
+
+function write(text, func, id){
+    var index = 0
+    function writer(){
+        var input = document.getElementById(id);
+        input.innerHTML += text.charAt(index);
+
+        index += 1;
+
+        if (text.length <= index){
+            setTimeout(func, 100);
+            return;
+        }
+        setTimeout(writer, 100);
+    }
+    writer()
+}
+
+write("./main.bin", ()=>{
+    document.getElementById("bash-promp2-view").style = "";
+
+    var main = document.getElementById("main");
+    main.style = "display: block";
+
+    var title = document.createElement("h3");
+    title.className = "title";
+    title.innerText = "Game";
+    
+    var childs = main.children;
+
+    var canvas = childs.item(0);
+    main.children = [];
+    main.appendChild(title);
+    main.appendChild(canvas);
+
+    write(
+        "clear",
+        ()=>{
+            setTimeout(() => {
+                clearBash();
+                write("cat ./profile.txt", 
+                    ()=>{
+                        var result = document.getElementById("result");
+                        result.style = "display: block;";
+
+                        var bash = document.getElementById("bash-promp2-view").style = "display: block";
+                    }, "bash-promp"); 
+            }, 100);
+        }, "bash-promp2"
+    );
+}, "bash-promp");
